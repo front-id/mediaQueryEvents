857501327ef183fedfcd0d19074baf47c293ccb5
Marcelo Tosco @capynet <marcelo@front.id>
2019-09-10 18:53:09 +0200

    Simplify the repo. Added samples.

--- mediaQueryEvents.jquery.js
+++ mediaQueryEvents.jquery.js
@@ -1,58 +1,82 @@
 /* ====================================================
  * jQuery Media Query Events.
- * https://github.com/front-id/mediaQueryEvents
+ * https://github.com/frontid/mediaQueryEvents
  * Marcelo Iván Tosco (capynet)
  * ==================================================== */
 !function ($) {
   'use strict';
 
-  var plugin;
+  let singleton;
 
-  var Class = function (win, options) {
-    plugin = this;
-    this.$win = $(win);
-    var defaults = {};
-    this.options = $.extend(defaults, options);
-    init();
-    return this;
+  const MediaQueryEventsClass = function ($el, options) {
+    singleton = this;
+    this.$el = $el;
+    this.options = $.extend({}, options);
+    this.map();
   };
 
-  function init() {
-    for (var brk in plugin.options.breakpoints) {
-      // Create the matchMedia and init.
-      var mql = window.matchMedia(plugin.options.breakpoints[brk]);
+  MediaQueryEventsClass.prototype = {
 
-      // Listen.
-      var listener = function (breakpointName) {
-        return function (e) {
+    /**
+     * Map each defined breakpoint with a listener.
+     */
+    map: function () {
+      const self = this;
+
+      for (let brkNameSpace in self.options.breakpoints) {
+        // Create the matchMedia and init.
+        const mql = window.matchMedia(self.options.breakpoints[brkNameSpace]);
+
+        const listener = function (e) {
           if (e.matches) {
-            plugin.$win.trigger('mq.' + breakpointName)
+            self._fire(brkNameSpace);
           }
+        };
+
+        mql.addListener(listener);
+
+        // Since the listener will not get triggered
+        // on init we make an initial check.
+        if (mql.matches) {
+          self._fire(brkNameSpace);
         }
-      };
 
-      mql.addListener(listener(brk));
-
-      // Since the listener will not get triggered
-      // on init we make an initial check.
-      if (mql.matches) {
-        plugin.$win.trigger('mq.' + brk)
       }
+    },
 
+    _fire: function (brkNameSpace) {
+      const self = this;
+      self.$el.trigger('mq.' + brkNameSpace);
+
+      console.log('-------------------------');
+      console.log(self.$el);
+      console.log(brkNameSpace);
+      console.log('-------------------------');
+    },
+
+    /**
+     * Updated the plugin with new definitions.
+     *
+     * This happens when a dom elements gets this plugin attached more than
+     * once. In this case we just add the new breakpoints to the stack.
+     *
+     * @param options
+     */
+    _update: function (options) {
+      const self = this;
+      this.options = $.extend(self.options, options);
+      self.map();
     }
 
-  }
+  };
 
-  // DEFINICIÓN DEL PLUGIN
+
+  // Jquery plugin definition.
   //-----------------------------------------------------------
   $.fn.mediaQueryEvents = function (options) {
-    return this.each(function () {
-      var $element = $(this);
-      var data = $element.data('mediaQueryEvents');
-      if (!data) {
-        $element.data('mediaQueryEvents', (new Class(this, options)))
-      }
-    })
+    const $el = $(this);
+    const instance = new MediaQueryEventsClass($el, options);
+    $el.data('MediaQueryEventsClass', instance);
   }
 
 }(window.jQuery);
--- sample/js/sample.js
+++ sample/js/sample.js
@@ -1,31 +1,52 @@
 $(function () {
 
-  // Define here the elements and the positions will
-  // take for each breakpoint. False means original position.
-  new DOMElementRelocation(
-    {
-      breakpoints: {
-        desk: '(min-width: 1230px)',
-        tab: '(min-width: 768px) and (max-width: 1229px)',
-        mob: '(min-width: 320px) and (max-width: 767px)',
-        reallySmall: '(min-width: 0px) and (max-width: 319px)'
-      },
-      elements: [
-        {
-          element: '.search-form',
-          reallySmall: '.sample-page > footer',
-          mob: '.sample-page > .region-sidebar-right',
-          tab: '.region-sidebar-left',
-          desk: '.sample-page > header'
-        },
-        {
-          element: '.logo',
-          reallySmall: '.sample-page > footer',
-          mob: false,
-          tab: '.region-sidebar-left',
-          desk: false,
-        }
-      ]
-    });
+  const $target = $('.target');
+  const $info = $target.find('.current-breakpoint');
+
+  const $target2 = $('.target2');
+  const $info2 = $target2.find('.current-breakpoint');
+
+  $target.on('mq.mob', function (e) {
+    $info.text('mob!');
+  });
+
+  $target.on('mq.somebreakpoint', function (e) {
+    $info.text('Another breakpoint!');
+  });
+
+  $target.on('mq.desk', function (e) {
+    $info.text('desk!');
+  });
+
+
+  // Sample 2. much more breakpoints.
+  $target2.on('mq.brk1 mq.brk2 mq.brk3 mq.brk4 mq.brk5 mq.brk6 mq.brk7 mq.brk8 mq.brk9', function (e) {
+    console.log(e);
+    $info2.text(e.namespace);
+  });
+
+
+  $target.mediaQueryEvents({
+    breakpoints: {
+      "mob": '(min-width: 0px) and (max-width: 700px)',
+      "somebreakpoint": '(min-width: 701px) and (max-width: 1023px)',
+      "desk": '(min-width: 1024px)'
+    }
+  });
+
+  $target2.mediaQueryEvents({
+    breakpoints: {
+      "brk1": '(min-width: 0px) and (max-width: 500px)',
+      "brk2": '(min-width: 501px) and (max-width: 600px)',
+      "brk3": '(min-width: 601px) and (max-width: 700px)',
+      "brk4": '(min-width: 701px) and (max-width: 800px)',
+      "brk5": '(min-width: 801px) and (max-width: 900px)',
+      "brk6": '(min-width: 901px) and (max-width: 1000px)',
+      "brk7": '(min-width: 1001px) and (max-width: 1100px)',
+      "brk8": '(min-width: 1101px) and (max-width: 1200px)',
+      "brk9": '(min-width: 1201px)'
+    }
+  });
+
 
 });
